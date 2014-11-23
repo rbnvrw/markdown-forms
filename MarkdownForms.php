e7186b3dc0c1d6327246f7385cfc2c1a7c94182c
Ruben Verweij <rbnvrw@gmail.com>
2014-11-23 13:44:59 +0100

    Create MarkdownForms.php

---
+++ MarkdownForms.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package Markdown
+* @subpackage Markdown Forms
+* @version 1.0
+* @author Ruben Verweij <development@rubenverweij.nl>
+* @link https://github.com/rbnvrw/markdown-forms
+* @license http://opensource.org/licenses/MIT
+*
+*/
+
+namespace RubenVerweij;
+
+class MarkdownForms extends \Michelf\MarkdownExtra {
+
+}
