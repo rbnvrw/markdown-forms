100aafc8fac586769ab38b448bfa0baec234496c
Ruben Verweij <development@rubenverweij.nl>
2014-11-24 12:26:44 +0100

    Fixed regex, now works for basic inputs and textareas

--- MarkdownForms.php
+++ MarkdownForms.php
@@ -16,16 +16,14 @@ class MarkdownForms extends \Michelf\MarkdownExtra {
 	private $sInputGroupTemplate = '
 	<div class="form-group">
 		<label for="{md_name}">{md_label}</label>
-		<input type="{md_type}" name="{md_name}" value="{md_value}" placeholder="{md_placeholder}" {md_attribs}>
+		<input type="{md_type}" id="{md_name}" name="{md_name}" value="{md_value}" placeholder="{md_placeholder}" {md_attribs}>
 	</div>
 	';
 	
 	private $sTextareaGroupTemplate = '
 	<div class="form-group">
 		<label for="{md_name}">{md_label}</label>
-		<textarea name="{md_name}" rows="{md_rows}" cols="{md_cols}" {md_attribs}>
-			{md_value}
-		</textarea>
+		<textarea name="{md_name}" id="{md_name}" rows="{md_rows}" cols="{md_cols}" placeholder="{md_placeholder}" {md_attribs}>{md_value}</textarea>
 	</div>
 	';
 		
@@ -55,68 +53,54 @@ class MarkdownForms extends \Michelf\MarkdownExtra {
 	# Turn Markdown input shortcuts into <input> tags.
 	#
 		#
-		# First, handle inline inputs:  ?[type]("label" "value" "placeholder" rows*cols){#id .class}
+		# First, handle inline inputs:  ?{type}("label" "value" "placeholder" rows*cols){.class}
 		# Don't forget: encode * and _
 		#
-		$text = preg_replace_callback('{
-			(				# wrap whole match in $1
-			  ?\[
-				('.$this->nested_brackets_re.')		# type = $2
-			  \]
-			  \s?			# One optional whitespace character
-			  \(			# literal paren
-				[ \n]*
-				(			# $3
-				  ([\'"])	# quote char = $4
-				  (.*?)		# label = $5
-				  \4		# matching quote
-				  [ \n]*
-				)			
-				[ \n]*
-				(			# $6
-				  ([\'"])	# quote char = $7
-				  (.*?)		# value = $8
-				  \7		# matching quote
-				  [ \n]*
-				)?			# value is optional			
-				[ \n]*
-				(			# $9
-				  ([\'"])	# quote char = $10
-				  (.*?)		# placeholder = $11
-				  \10		# matching quote
-				  [ \n]*
-				)?			# placeholder is optional
-				[ \n]*
-				(			# $12
-				([0-9]+)	# rows = $13
-				[ \n]*
-				\*
-				[ \n]*
-				([0-9]+)	# cols = $14
-				[ \n]*
-				)?			# rows*cols is optional
-			  \)
-			  (?:[ ]? '.$this->id_class_attr_catch_re.' )?	 # $15 = id/class attributes
-			)
-			}xs',
-			array($this, '_doInputs_callback'), $text);
+		
+		/*
+		\?\{
+		\s*(\w+)\s*                     # $1 = type
+		\}
+		
+		\(\s*(([\'\"])(.*?)\3)?         # $4 = label
+		\s*(([\'\"])(.*?)\6)?           # $7 = value
+		\s*(([\'\"])(.*?)\9)?           # $10 = placeholder
+		\s*((\d+)\*(\d+))?              # $12 = rows, $13 = cols
+		\s*\)
+		
+		(\{(.*?)\})?                    # $15 = extra attributes
+		*/
+		$text = preg_replace_callback('
+		/
+		\?\{
+		\s*(\w+)\s*
+		\}
+		
+		\(\s*(([\'\"])(.*?)\3)?
+		\s*(([\'\"])(.*?)\6)?
+		\s*(([\'\"])(.*?)\9)?
+		\s*((\d+)\*(\d+))?
+		\s*\)
+		
+		(\{(.*?)\})?
+		/xs', array($this, '_doInputs_callback'), $text);
 
 		return $text;
 	}
 	
 	protected function _doInputs_callback($matches) {
-		$whole_match = $matches[1];
-		$type = $this->encodeAttribute($matches[2]);
-		$label = $this->encodeAttribute($matches[5]);
-		$value = $this->encodeAttribute($matches[8]);
-		$placeholder = $this->encodeAttribute($matches[11]);
+		$whole_match = $matches[0];
+		$type = $this->encodeAttribute(trim($matches[1]));
+		$label = $this->encodeAttribute(trim($matches[4]));
+		$value = $this->encodeAttribute(trim($matches[7]));
+		$placeholder = $this->encodeAttribute(trim($matches[10]));
 		
 		if($type != "textarea"){
 			$attr = $this->doExtraAttributes("input", $dummy =& $matches[15]);
 		}else{
 			$attr = $this->doExtraAttributes("textarea", $dummy =& $matches[15]);
-			$rows = $this->encodeAttribute($matches[13]);
-			$cols = $this->encodeAttribute($matches[14]);
+			$rows = $this->encodeAttribute(trim($matches[12]));
+			$cols = $this->encodeAttribute(trim($matches[13]));
 		}
 		
 		if($type != "textarea"){	
